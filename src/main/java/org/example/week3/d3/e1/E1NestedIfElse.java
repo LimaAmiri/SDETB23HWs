0f982441ce956ff965877761a4b837f4b33372bd
LimaAmiri <limaamiri30@gmail.com>
2025-04-30 20:42:25 -0400

    Update E1NestedIfElse.java

--- src/main/java/org/example/week3/d3/e1/E1NestedIfElse.java
+++ src/main/java/org/example/week3/d3/e1/E1NestedIfElse.java
@@ -3,15 +3,19 @@ package org.example.week3.d3.e1;
 public class E1NestedIfElse {
     public static void main(String[] args) {
 
-        // Create age and height variables, assign values to age and height (age = 14, height = 125)
-        // Check if age is greater than or equal to 12:
-        //    If true, then check if height is greater than or equal to 120:
-        //         If true, print "You are eligible to ride the roller coaster."
-        //         Else, print "You are old enough, but not tall enough to ride the roller coaster."
-        // Else, print "You are not old enough to ride the roller coaster."
 
-
-
-
+        // Check if age is greater than or equal to 12
+        if (age >= 12) {
+            // Nested if: check if height is greater than or equal to 120
+            if (height >= 120) {
+                System.out.println("You are eligible to ride the roller coaster.");
+            } else {
+                System.out.println("You are old enough, but not tall enough to ride the roller coaster.");
+            }
+        } else {
+            System.out.println("You are not old enough to ride the roller coaster.");
+        }
     }
 }
+
+       
