efff055d2ee802d2666ed6389e99b7ceb819f34c
LimaAmiri <limaamiri30@gmail.com>
2025-04-30 21:07:06 -0400

    Update E1SwitchClass.java

--- src/main/java/org/example/week3/d5/e1/E1SwitchClass.java
+++ src/main/java/org/example/week3/d5/e1/E1SwitchClass.java
@@ -1,18 +1,36 @@
+
 package org.example.week3.d5.e1;
+
+import java.util.Scanner;
 
 public class E1SwitchClass {
     public static void main(String[] args) {
         // Create a Scanner object to read input
+        Scanner input = new Scanner(System.in);
 
-        // Print prompt for user to enter the name of the instructor
-        // Capture the instructor name input
+        // Prompt user to enter the name of the instructor
+        System.out.println("Enter name of the instructor");
+        String instructorName = input.nextLine();
+
+        // Initialize a variable to hold the responsibility message
+        String responsibility;
 
         // Determine the responsibility based on the instructor's name using a switch statement
+        switch (instructorName) {
+            case "Asghar":
+                responsibility = "Will take care of Java Assignment";
+                break;
+            case "Sohail":
+                responsibility = "Will take care of SDLC Assignment";
+                break;
+            case "Moazzam":
+                responsibility = "Will take care of Selenium Assignment";
+                break;
+            case "Asel":
+                responsibility = "Will take care of every Assignment";
+                break;
+            default:
+                responsibility = "Invalid instructor selected";
+        }
 
-
-        // Print the responsibility
-
-
-
-    }
 }
